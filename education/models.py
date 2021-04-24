ce09a99b232dfca24833eaf9bb0b67b85eec5ecf
JakariaPUST <jakaria.pust@gmail.com>
2021-04-24 16:00:41 +0600

    Post model added in Model and Asia/Dhaka time zone added

--- education/models.py
+++ education/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.timezone import now
 class Contact(models.Model):
     name = models.CharField(max_length=150)
     email = models.CharField(max_length=150)
@@ -8,3 +8,21 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+class Post(models.Model):
+    CATEGORY={
+        ('Teacher', 'Teacher'),
+        ('Student', 'Student'),
+    }
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, default=title)
+    email = models.EmailField()
+    salary = models.FloatField()
+    details = models.TextField()
+    available = models.BooleanField()
+    category = models.CharField(max_length=100, choices=CATEGORY)
+    created_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.title
