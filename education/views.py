964144831248be9f4d1fc950cee8a6c22520dbd5
JakariaPUST <jakaria.pust@gmail.com>
2021-04-24 15:37:25 +0600

    djagno model form + Crispy form

--- education/views.py
+++ education/views.py
@@ -6,13 +6,7 @@ def contact(request):
     if request.method == "POST":
         form=ContactForm(request.POST)
         if form.is_valid():
-            name=form.cleaned_data['name']
-            email=form.cleaned_data['email']
-            phone=form.cleaned_data['phone']
-            content=form.cleaned_data['content']
-            
-            obj = Contact(name=name, email=email, phone=phone, content=content)
-            obj.save()
+            form.save()
     else:
         form= ContactForm()
 
