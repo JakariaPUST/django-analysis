cf2fb7573e598129c94ccdee823c5f1454488cfd
JakariaPUST <jakaria.pust@gmail.com>
2021-04-26 12:26:53 +0600

    User Registration

--- session/urls.py
+++ session/urls.py
@@ -1,12 +1,11 @@
 
 from django.urls import path
-from education.views import postview, postcreate, subjectview, ContactView, postView, postDetailView, postEditView, postDeleteView, searchresult, filter
-from education.forms import ContactForm2
-from .views import loginuser, logoutuser
+from .views import loginuser, logoutuser, registration
 app_name='session'
 
 urlpatterns = [
     path('login/',loginuser , name="login"),
     path('logout/',logoutuser , name="logout"),
+    path('signup/',registration , name="signup"),
 
 ]
--- session/views.py
+++ session/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def loginuser(request):
@@ -27,3 +28,14 @@ def logoutuser(request):
     logout(request)
     messages.success(request, "Successfully logged Out!")
     return redirect("home")
+
+
+def registration(request):
+    if request.method=="POST":
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('session:login')
+    else:
+        form=SignUpForm()
+    return render(request, 'session/signup.html', {'form': form})
